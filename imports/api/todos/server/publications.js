8e9fbb4f458b2f01f001d308b9f17e89b0464f05
baryshok <baryshok@gmail.com>
2020-03-19 21:18:34 +0300

    Alter todos.inList publication so that it uses a server-side autorun with a computed field inside

--- imports/api/todos/server/publications.js
+++ imports/api/todos/server/publications.js
@@ -2,42 +2,47 @@
 
 import { Meteor } from 'meteor/meteor';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { ComputedField } from 'meteor/peerlibrary:computed-field';
 
 import { Todos } from '../todos.js';
 import { Lists } from '../../lists/lists.js';
 
-Meteor.publishComposite('todos.inList', function todosInList(params) {
+
+Meteor.publish('todos.inList', function todosInList(params) {
   new SimpleSchema({
     listId: { type: String },
   }).validate(params);
 
-  const { listId } = params;
-  const { userId } = this;
+  const getList = new ComputedField(() => {
+    const { listId } = params;
+    const { userId } = this;
 
-  return {
-    find() {
-      const orSelectors = [{ userId: { $exists: false } }];
-      if (userId) {
-        orSelectors.push({ userId });
-      }
-      const query = {
-        _id: listId,
-        $or: orSelectors,
-      };
+    const orSelectors = [{ userId: { $exists: false }}];
+    if (userId) {
+      orSelectors.push({ userId });
+    }
 
-      // We only need the _id field in this query, since it's only
-      // used to drive the child queries to get the todos
-      const options = {
-        fields: { _id: 1 },
-      };
+    const query = {
+      _id: listId,
+      $or: orSelectors,
+    };
 
-      return Lists.find(query, options);
-    },
+    const options = {
+      fields: { _id: 1 },
+      sort: { _id: 1 },
+      ordered: false,
+    };
 
-    children: [{
-      find(list) {
-        return Todos.find({ listId: list._id }, { fields: Todos.publicFields });
-      },
-    }],
-  };
+    return Lists.findOne(query, options);
+  });
+
+  this.autorun(() => {
+    const list = getList();
+    if (!list) return this.ready();
+    return Todos.find({ listId: list._id }, { fields: Todos.publicFields });
+  });
+
+  // this.onStop(() => {
+  //   getList.stop();
+  // });
 });
